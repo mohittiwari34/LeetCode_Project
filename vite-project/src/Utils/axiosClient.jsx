d0101dcbfb25fd3499d79ec4bf23372bdedca38a
Mohit Tiwari <mohittiwarigcmb@gmail.com>
2026-01-28 14:20:19 +0530

    render url added

--- vite-project/src/Utils/axiosClient.jsx
+++ vite-project/src/Utils/axiosClient.jsx
@@ -1,13 +1,19 @@
 import axios from "axios"
 
+// const axiosClient =  axios.create({
+//     baseURL: 'http://localhost:3000',
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// });
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://leetcode-project-2-19fy.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
 });
-
 
 export default axiosClient;
 
